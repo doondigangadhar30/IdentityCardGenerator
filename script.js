7335b170907b888144a55340d821d037da606833
R DOONDI GANGADHAR <160998038+doondigangadhar30@users.noreply.github.com>
2024-10-19 00:48:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function generateID() {
+    const name = document.getElementById('name').value;
+    const branch = document.getElementById('branch').value;
+    const roll = document.getElementById('roll').value;
+    const email = document.getElementById('email').value;
+    const birthday =document.getElementById('birthday').value;
+    const blood =document.getElementById('blood').value;
+    const address = document.getElementById('address').value;
+    const year = parseInt(document.getElementById('year').value);
+    const endYear = year + 4;
+    const photo = document.getElementById('photo').files[0];
+
+    if (name && branch && roll && email && address && year && photo) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const photoURL = e.target.result;
+            const idCardData = {
+                name: name,
+                branch: branch,
+                roll: roll,
+                email: email,
+                birthday: birthday,
+                blood: blood,
+                address: address,
+                year: `${year}-${endYear}`,
+                photo: photoURL
+            };
+
+            localStorage.setItem('idCardData', JSON.stringify(idCardData));
+            window.open('id-generation.html');
+        };
+        reader.readAsDataURL(photo);
+    } else {
+        alert('Please fill in all the details and upload a photo.');
+    }
+}
+
+window.onload = function() {
+    const idCardData = JSON.parse(localStorage.getItem('idCardData'));
+
+    if (idCardData) {
+        document.getElementById('studentName').innerText = idCardData.name;
+        document.getElementById('studentBranch').innerText = idCardData.branch;
+        document.getElementById('studentRoll').innerText = idCardData.roll;
+        document.getElementById('studentEmail').innerText = idCardData.email;
+        document.getElementById('studentBirthday').innerText = idCardData.birthday;
+        document.getElementById('studentBlood').innerText = idCardData.blood;
+        document.getElementById('studentAddress').innerText = idCardData.address;
+        document.getElementById('studentPhoto').src = idCardData.photo;
+        document.getElementById('studentYear').innerText = idCardData.year;
+    }
+};
